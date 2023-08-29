1e83a844e530eb88ccca53c3d6fcb2d2c493af1a
thirupathi1705 <143484170+thirupathi1705@users.noreply.github.com>
2023-08-29 12:21:28 +0530

    Delete djproject directory

--- djproject/userapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from.models import UserDetails
-
-admin.site.register(UserDetails)
--- djproject/userapp/from.py
+++
@@ -1,9 +0,0 @@
-from django.from import ModelsFrom
-
-
-from.models import UserDetalis
-
-
-class UserFrom(ModelFrom):
-       class meta:
-        models1=UserDetalis
